58555a4e5a1e75d9bb53f4cecf851177f475cb81
Amyn617 <aarfaoui617@gmail.com>
2025-12-17 20:10:11 +0100

    Strategy exception for default implementation

--- src/strategy/Main.java
+++ src/strategy/Main.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) {
         Context context = new Context();
         Scanner scanner = new Scanner(System.in);
         Map<String, Strategy> strategyMap = new HashMap<>();
@@ -16,9 +16,15 @@ public class Main {
             String str = scanner.nextLine();
             strategy = strategyMap.get(str);
             if (strategy == null) {
-                System.out.println("Creation d'un nouvel objet de StrategyImpl" + str);
-                strategy = (Strategy) Class.forName("strategy.StrategyImpl" + str).getConstructor().newInstance();
-                strategyMap.put(str, strategy);
+                try {
+                    System.out.println("Creation d'un nouvel objet de StrategyImpl" + str);
+                    strategy = (Strategy) Class.forName("strategy.StrategyImpl" + str).getConstructor().newInstance();
+                    strategyMap.put(str, strategy);
+                } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | 
+                         InstantiationException | IllegalAccessException e) {
+                    System.out.println("Strategie " + str + " n'existe pas, utilisation de la strategie par defaut");
+                    strategy = new DefaultStrategyImpl();
+                }
             }
             context.setStrategy(strategy);
             context.effectuerOperation();
